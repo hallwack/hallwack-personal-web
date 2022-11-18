6611f859c205298cbfd41b4b505da7fb9e3d92b8
Raihan Adam <hallwack.id@gmail.com>
2022-11-18 13:21:36 +0700

    feat: add portofolioCard component

---
+++ src/components/PortofolioCard/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+
+function PortofolioCard({ title, description, link }) {
+  return (
+    <div className="bg-sky-200 p-8 rounded-lg flex max-w-sm gap-4">
+      <div className="mr-4">
+        <FaGithub className="fill-sky-800" size={48} />
+      </div>
+      <div className="flex flex-col">
+        <h2 className="text-sky-800 font-semibold text-2xl">{title}</h2>
+        <p className="text-slate-600 mb-6">{description}</p>
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="px-3 py-2 rounded hover:text-slate-800 font-medium hover:bg-sky-300 bg-sky-700 text-slate-100 transition hover:duration-300 duration-300 w-auto self-end flex items-center"
+        >
+          Show Repository
+          <FaExternalLinkAlt className="ml-1" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default PortofolioCard;
