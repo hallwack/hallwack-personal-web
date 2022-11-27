4843810487fdd68aff580dd47f636cc8935e0fd0
Raihan Adam <hallwack.id@gmail.com>
2022-11-27 17:52:09 +0700

    fix: fixing styling in achievementCard component

--- src/pages/Achievement.js
+++ src/pages/Achievement.js
@@ -6,13 +6,13 @@ import { achievements } from "../data/data";
 
 function Achievement() {
   return (
-    <div className="flex flex-col bg-sky-50 h-full">
+    <div className="flex flex-col bg-sky-50 min-h-screen">
       <Navbar />
-      <section className="container mx-auto px-12 flex flex-col gap-8 justify-center items-center flex-grow">
+      <section className="container mx-auto px-12 py-4 flex flex-col gap-8 justify-center items-center flex-grow">
         <h1 className="text-4xl font-bold text-sky-800 text-center">
           Achievement
         </h1>
-        <div className="grid grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-12">
           {achievements.map((achievement, key) => (
             <AchievementCard
               key={key}
